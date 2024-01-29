9e78ce4d5c52662ec0b1395af419dbb6143bffba
Greg Lyon <gregslyon@gmail.com>
2024-01-29 13:51:01 -0600

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'MPGPeregrine'
+copyright = '2023, MPG'
+author = 'MPG'
 
 release = '0.1'
 version = '0.1.0'
